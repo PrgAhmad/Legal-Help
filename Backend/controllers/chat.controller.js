9eca2441da1a92c155ebeeecda64e9a8d4a7c590
PrgAhmad <ahmadrazaansari340@gmail.com>
2026-01-08 09:27:49 +0530

    added gemini ai response

--- Backend/controllers/chat.controller.js
+++ Backend/controllers/chat.controller.js
@@ -1,7 +1,8 @@
 import { SYSTEM_PROMPT, ANSWER_STYLE, ANSWER_TYPE } from "../chatbot_style.js";
+import { gemini } from "../gemini.js";
 import { summarize, getPrevious, randomSeed } from "../helpers/chat.helper.js";
 
-export const generateResponse = async ({ question, style, type }) => {
+export const generateResponse = async ({ question, style, type, isGemini }) => {
   const previous = getPrevious();
   const query =
     SYSTEM_PROMPT +
@@ -11,10 +12,17 @@ export const generateResponse = async ({ question, style, type }) => {
     previous +
     "\nUser Question : " +
     question;
-
   console.log(query);
-  const res = await fetch(`${process.env.AI_API_URL}/${query}&seed=${randomSeed()}`);
-  let data = await res.text();
+  let data;
+  if (isGemini == "true") {
+    data = await gemini(query);
+    console.log(data);
+  } else {
+    const res = await fetch(
+      `${process.env.AI_API_URL}/${query}&seed=${randomSeed()}`
+    );
+    data = await res.text();
+  }
   data = data.replaceAll("```", "").replaceAll("json", "");
   await summarize(question, data);
   return data;
