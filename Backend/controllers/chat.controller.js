72eb72d2aa73aa70db570a71309f8dce73134bdf
PrgAhmad <ahmadrazaansari340@gmail.com>
2026-02-13 18:36:27 +0530

    added PORT

--- Backend/controllers/chat.controller.js
+++ Backend/controllers/chat.controller.js
@@ -1,5 +1,5 @@
 import { SYSTEM_PROMPT, ANSWER_STYLE, ANSWER_TYPE } from "../chatbot_style.js";
-import { gemini } from "../gemini.js";
+// import { gemini } from "../gemini.js";
 import { summarize, getPrevious, randomSeed } from "../helpers/chat.helper.js";
 
 export const generateResponse = async ({ question, style, type, isGemini }) => {
@@ -15,7 +15,7 @@ export const generateResponse = async ({ question, style, type, isGemini }) => {
   console.log(query);
   let data;
   if (isGemini == "true") {
-    data = await gemini(query);
+    // data = await gemini(query);
     console.log(data);
   } else {
     const res = await fetch(
--- Backend/gemini.js
+++ Backend/gemini.js
@@ -1,13 +1,13 @@
-import { GoogleGenAI } from "@google/genai";
+// import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY,
-});
+// const ai = new GoogleGenAI({
+//   apiKey: process.env.GEMINI_API_KEY,
+// });
 
-export async function gemini(text) {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: text,
-  });
-  return response.candidates[0].content.parts[0].text;
-}
+// export async function gemini(text) {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: text,
+//   });
+//   return response.candidates[0].content.parts[0].text;
+// }
